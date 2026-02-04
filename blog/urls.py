751fc7d5ad4c1da79594b4860293a6ea9940e371
shubham33159 <shubham33159@gmailcom>
2026-02-04 22:19:56 +0530

    added class based view

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.starting_page, name="starting-page"),
-    path("posts", views.posts, name="posts-page"),
-    path("posts/<slug:slug>", views.post_detail, name="post-detail-page")  #/posts/my-first-post
-]
+    path("", views.StartingPageView.as_view(), name="starting-page"),
+    path("posts", views.PostsView.as_view(), name="posts-page"),
+    path("posts/<slug:slug>", views.PostDetail.as_view(), name="post-detail-page")  #/posts/my-first-post
+] 
--- blog/views.py
+++ blog/views.py
@@ -3,29 +3,132 @@ from datetime import date
 
 from blog.models import Post, Author, Tag
 
+from django.views.generic import ListView
+
+from django.views.generic import DetailView
+
 # Create your views here.
 
-# all_posts= []
+# all_posts= [
+#     {
+#         "slug": "hike-in-the-mountains",
+#         "image": "mountains.jpg",
+#         "author": "Maximilian",
+#         "date": date(2021, 7, 21),
+#         "title": "Mountain Hiking",
+#         "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
+#         "content": """
+#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+#         """
+#     },
+#     {
+#         "slug": "programming-is-fun",
+#         "image": "coding.jpg",
+#         "author": "Maximilian",
+#         "date": date(2022, 3, 10),
+#         "title": "Programming Is Great!",
+#         "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
+#         "content": """
+#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+#         """
+#     },
+#     {
+#         "slug": "into-the-woods",
+#         "image": "woods.jpg",
+#         "author": "Maximilian",
+#         "date": date(2020, 8, 5),
+#         "title": "Nature At Its Best",
+#         "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
+#         "content": """
+#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+#         """
+#     }
+# ]
 
 
-def starting_page(request):
-    latest_posts = Post.objects.all().order_by("-date")[:3]
+# def starting_page(request):
+#     latest_posts = Post.objects.all().order_by("-date")[:3]
 
-    return render(request, "blog/index.html", {
-        "posts" : latest_posts
-    })
+#     return render(request, "blog/index.html", {
+#         "posts" : latest_posts
+#     })
 
-def posts(request):
-    all_posts = Post.objects.all().order_by("-date")
-    return render(request, "blog/all-posts.html", {
-        "posts" : all_posts
-    })
 
-def post_detail(request, slug):
+
+# def posts(request):
+#     all_posts = Post.objects.all().order_by("-date")
+#     return render(request, "blog/all-posts.html", {
+#         "posts" : all_posts
+#     })
+
+# def post_detail(request, slug):
     
-    identified_post = Post.objects.get(slug=slug)
-    # identified_post = get_object_or_404(Post, slug=slug)
-    return render(request, "blog/post-details.html", {
-        "post" : identified_post,
-        "post_tags" : identified_post.tags.all()
-    })
+#     identified_post = Post.objects.get(slug=slug)
+#     # identified_post = get_object_or_404(Post, slug=slug)
+#     return render(request, "blog/post-details.html", {
+#         "post" : identified_post,
+#         "post_tags" : identified_post.tags.all()
+#     })
+
+class StartingPageView(ListView):
+    template_name = "blog/index.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
+    
+    
+class PostsView(ListView):
+    template_name = "blog/all-posts.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "posts"
+    
+    
+
+class PostDetail(DetailView):
+    template_name = "blog/post-details.html"
+    model = Post
+
+    def get_context_data(self, **kwargs) -> dict[str, any]:
+        context = super().get_context_data(**kwargs)
+        # slug_id = self.kwargs["slug"]
+        # context["post"] = self.model.objects.get(slug=slug_id)
+
+        context["post_tags"] = self.object.tags.all()
+        return context
+    
